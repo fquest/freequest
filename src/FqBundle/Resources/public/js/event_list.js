c314356e67b10dafa7c61acdc7e4c455bb5ee2d3
Sergey Ivashchenko <sivashchenko@ebay.com>
2015-01-16 13:21:46 +0200

    Enabled search by Enter key press

--- src/FqBundle/Resources/public/js/event_list.js
+++ src/FqBundle/Resources/public/js/event_list.js
@@ -10,3 +10,14 @@ $('[data-role="event-list-reloader"]').click(function() {
     }
     $('#list_filter').submit();
 });
+$('[data-role="search"]').keyup(function(event){
+    if(event.keyCode == 13){
+        var searchQuery = $('[data-role="search"]').val();
+        if (searchQuery) {
+            var input = $('<input type="text" name="query" hidden="hidden">');
+            input.val(searchQuery);
+            $('#list_filter').append(input);
+        }
+        $('#list_filter').submit();
+    }
+});
