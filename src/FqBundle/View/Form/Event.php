f72e50dc91da59360ee7166bade73f0e37b8412a
Sergey Ivashchenko <serg.ivashchenko@gmail.com>
2015-01-21 21:54:36 +0200

    #24362355625372 Cosmetic fixes

--- src/FqBundle/View/Form/Event.php
+++ src/FqBundle/View/Form/Event.php
@@ -6,9 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-/**
- * @Route("/event")
- */
 class Event extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
--- src/FqBundle/View/Form/Route.php
+++ src/FqBundle/View/Form/Route.php
@@ -6,9 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-/**
- * @Route("/event")
- */
 class Route extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
